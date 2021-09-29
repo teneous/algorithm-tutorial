be263e647e282347c09316258e6814cd63bd65d2
肖恺 <xiaok@paraview.cn>
2021-09-29 09:24:57 +0800

    T# 日志注释调整

--- leecode/2_水王问题_test.go
+++ leecode/2_水王问题_test.go
@@ -1,4 +1,4 @@
-// 水王问题，一个数在一个集合内大于一半，请用时间复杂度为o(n),空间复杂度为o(1)找到这个书
+// 水王问题，一个数在一个集合内大于一半，找到这个水王数。请用时间复杂度为o(n),空间复杂度为o(1)找到这个树
 package leecode
 
 import (
@@ -6,15 +6,18 @@ import (
 	"testing"
 )
 
+// 解题思路，如果存在一个水王数，那么它在集合内的数量必定超过一半。在遍历集合的时候，从头元素开始，初始hp为1，
+// - 如果当前元素和下一个元素不等，那么当前遍历节点元素hp-1
+// - 如果当前元素和下一个元素相等，那么当前遍历节点元素hp+1
+// - 如果当前hp等于0，则下一个元素的成为新的候选者，赋值hp=1
 func TestRun(t *testing.T) {
-	// 解题思路，如果存在一个水王数，那么它在集合内的数量必定超过一半。在遍历集合的时候，从头元素开始，默认hp为1，
-	// - 如果当前元素和下一个元素不等，那么当前遍历节点元素hp-1，并跳过这两个元素
-	// - 如果当前元素和下一个元素相等，那么当前遍历节点元素hp+1，并跳到下一个元素
-	// 例如 1，2，3，3，4
-
 	// 测试数据
 	source := []int{1, 2, 3, 3, 3, 4, 2, 3, 2, 3, 3}
+	// 找到水王货
+	catchFakeOne(source)
+}
 
+func catchFakeOne(source []int) {
 	hp := 0
 	candidate := 0
 
@@ -32,7 +35,7 @@ func TestRun(t *testing.T) {
 	}
 
 	if hp == 0 {
-		fmt.Printf("不存在水王数")
+		fmt.Printf("不存在水王数\n")
 	} else {
 		count := 0
 		for _, val := range source {
@@ -41,9 +44,10 @@ func TestRun(t *testing.T) {
 			}
 		}
 		if count > (len(source) >> 1) {
-			fmt.Printf("存在水王数:%d,个数%d", candidate, count)
+			fmt.Printf("存在水王数，它是：%d，个数：%d个\n", candidate, count)
 		} else {
-			fmt.Printf("不存在水王数")
+			fmt.Printf("不存在水王数\n")
 		}
 	}
+
 }
---
+++ leecode/3_从整数池中获取出现奇数次的数_test.go
@@ -0,0 +1,9 @@
+package leecode
+
+import "testing"
+
+// 从整数池中获取出现奇数次的数
+// 例如「1,3,4,4,2,3,6」 计算最终得到1和6
+func TestGetDiffEvenNumberFromPool(t *testing.T) {
+
+}
